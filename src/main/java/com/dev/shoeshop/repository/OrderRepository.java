0d03e3d1089e0dddc62fd7a1105c04e738d7c668
Truong Nhat Nguyen <tnhatnguyen.dev2805@gmail.com>
2025-10-15 20:42:29 +0700

    Update interface for shipper

--- src/main/java/com/dev/shoeshop/repository/OrderRepository.java
+++ src/main/java/com/dev/shoeshop/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.dev.shoeshop.repository;
 
 import com.dev.shoeshop.entity.Order;
 import com.dev.shoeshop.enums.ShipmentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -13,6 +15,8 @@ public interface OrderRepository extends JpaRepository<Order,Long> {
     Long countByStatus(ShipmentStatus status);
 
     public List<Order> findByStatus(ShipmentStatus status);
+    
+    public Page<Order> findByStatus(ShipmentStatus status, Pageable pageable);
 
     public Order findOrderById(Long id);
 
